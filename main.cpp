54d4245d04282e7666c02730115791c78a51e729
vyacheslav.ryndin <vyacheslav.ryndin@matryoshka.com>
2021-12-27 22:21:59 +0300

    Basic instalations

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+#include <time.h>
+
+int mapWidth = 20;
+int mapHeight = 20;
+int size = 32;
+int width = size * mapWidth;
+int height = size * mapHeight;
+
+int main()
+{
+
+
+	return 0;
+}
